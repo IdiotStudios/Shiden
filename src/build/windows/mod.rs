0aa50dfc852c72496b803885b7b0cbb4d2cbcf4a
SeradedStripes <seradedstripes@gmail.com>
2026-03-01 12:46:07 +0200

    fix 7

--- src/build/windows/mod.rs
+++ src/build/windows/mod.rs
@@ -438,6 +438,30 @@ pub fn write_executable_from_sections(
 
                     let file_off = pe.len() + off;
                     file_bytes[file_off..file_off + 8].copy_from_slice(&le);
+                } else if s == "__argc" {
+                    let off = r.offset;
+                    let argc_va = IMAGE_BASE + rdata_rva + (argc_ro_offset as u64);
+                    let le = argc_va.to_le_bytes();
+                    text[off..off + 8].copy_from_slice(&le);
+
+                    let file_off = pe.len() + off;
+                    file_bytes[file_off..file_off + 8].copy_from_slice(&le);
+                } else if s == "__argv" {
+                    let off = r.offset;
+                    let argv_va = IMAGE_BASE + rdata_rva + (argv_ro_offset as u64);
+                    let le = argv_va.to_le_bytes();
+                    text[off..off + 8].copy_from_slice(&le);
+
+                    let file_off = pe.len() + off;
+                    file_bytes[file_off..file_off + 8].copy_from_slice(&le);
+                } else if s == "__argv_store" {
+                    let off = r.offset;
+                    let argv_store_va = IMAGE_BASE + rdata_rva + (argv_store_ro_offset as u64);
+                    let le = argv_store_va.to_le_bytes();
+                    text[off..off + 8].copy_from_slice(&le);
+
+                    let file_off = pe.len() + off;
+                    file_bytes[file_off..file_off + 8].copy_from_slice(&le);
                 }
             }
         }
